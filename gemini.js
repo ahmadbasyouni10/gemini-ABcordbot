8f1660824a80a6fc4c8a6b685e8dd27915266f65
Ahmad Basyouni <ahmadnayef2004@gmail.com>
2024-04-01 01:03:28 -0400

    Fixed Broken API key

---
+++ gemini.js
@@ -0,0 +1,18 @@
+const { GoogleGenerativeAI } = require('@google/generative-ai');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+console.log('API_KEY:', process.env.API_KEY);
+
+async function runGeminiPro(prompt) {
+    // For text-only input, use the gemini-pro model
+    const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    return text;
+  }
+
+module.exports = { runGeminiPro };
